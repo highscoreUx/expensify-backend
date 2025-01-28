0e242af60b122bd212095d086d530c1773607dd5
highscoreux <boyesiji@gmail.com>
2025-01-28 21:58:00 +0100

    feat(app): add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import { Express } from "express";
 import { appFactoryImpl } from ".";
+import { connectMongooseDb } from "./utilities/databaseConnection";
+import { configDotenv } from "dotenv";
 
+configDotenv();
 const port = process.env.PORT || 8000;
-const startServer = async (app: Express) => {
+const startServer = async (
+	app: Express,
+	connectDB: (URI: string) => Promise<void>,
+	URI: string,
+) => {
 	try {
+		await connectDB(URI);
 		app.listen(port, () => {
 			console.log(`Sever is listening on port`);
 		});
@@ -13,7 +21,7 @@ const startServer = async (app: Express) => {
 	}
 };
 
-await startServer(appFactoryImpl());
+await startServer(appFactoryImpl(), connectMongooseDb, process.env.DB_URL!);
 
 process.on("SIGINT", () => {
 	try {
