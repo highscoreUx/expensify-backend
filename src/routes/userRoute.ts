9669b98f68a45a03369fd7f2d60c23688cef48a6
highscoreux <boyesiji@gmail.com>
2025-01-28 21:23:24 +0100

    feat(user): add user route

---
+++ src/routes/userRoute.ts
@@ -0,0 +1,20 @@
+import { httpStatus } from "@/constants";
+import { UserRepositoryImpl } from "@/hexagon/adapters/driven/userRepositoryImp";
+import { UserControllerImpl } from "@/hexagon/adapters/driver/userControllerImpl";
+import { UserServiceImpl } from "@/hexagon/core/userServiceImpl";
+import User from "@/models/User";
+import { TUser } from "@/utilities/Zod";
+import Router from "express";
+import { Model } from "mongoose";
+
+const router = Router();
+
+const userRepository = new UserRepositoryImpl(
+	User as unknown as Model<TUser & Document>,
+);
+const userService = new UserServiceImpl(userRepository);
+const { signUp } = new UserControllerImpl(userService, httpStatus);
+
+router.post("/", signUp);
+
+export default router;
